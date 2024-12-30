58e315a8c48d22e4f6559599f88501ba402ac261
baanza <abyaclifford784@gmail.com>
2024-12-30 10:50:25 +0300

    multiple dependecies and are used on the path operator

---
+++ Dependency/path.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+from fastapi import Depends, FastAPI, Header, HTTPException, Cookie
+
+app = FastAPI()
+
+async def cookie_fetcher(cookie: Annotated[str, Cookie('just now')]):
+    if cookie == "trial":
+        raise HTTPException(status_code=418, detail="stop trying")
+    return cookie
+
+async def HeaderFetcher(key: Annotated[str, Header()]):
+    return key
+
+
+@app.get('/users/', dependencies=[Depends(cookie_fetcher), Depends(HeaderFetcher)])
+async def main():
+    return {
+        "depends": "On two functions"
+    }
