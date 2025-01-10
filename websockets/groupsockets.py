de15f7506343a75a7c30e36342a24cd174dcb6a4
baanza <abyaclifford784@gmail.com>
2025-01-10 11:53:47 +0300

    created a manager

--- websockets/groupsockets.py
+++ websockets/groupsockets.py
@@ -1,3 +1,31 @@
-from fastapi import WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
-class 
+class ConnectionManager:
+
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def direct_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+
+app = FastAPI()
+manager = ConnectionManager()
+
+@app.websocket("/communicate")
+async def socket(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.direct_message(f"message received", websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.direct_message("bye", websocket)
